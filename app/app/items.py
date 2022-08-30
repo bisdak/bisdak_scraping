0d79d84016296f81d6e45b7946de95bce65b5b1b
bisdak <krismar.ole@gmail.com>
2022-08-30 13:10:31 +0800

    added product model

--- app/app/items.py
+++ app/app/items.py
@@ -1,12 +1,14 @@
-# Define here the models for your scraped items
-#
-# See documentation in:
-# https://docs.scrapy.org/en/latest/topics/items.html
-
 import scrapy
 
 
-class AppItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class Product(scrapy.Item):
+    name = scrapy.Field()
+    product_line = scrapy.Field()
+    volume = scrapy.Field()
+    weight = scrapy.Field()
+    price = scrapy.Field()
+    article = scrapy.Field()
+    barcode = scrapy.Field()
+    composition = scrapy.Field()
+    photo = scrapy.Field()
+    source = scrapy.Field()
